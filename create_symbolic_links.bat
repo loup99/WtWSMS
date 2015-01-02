a7d4151f46fc6552e7939dc6d7895b6c92b8423e
romain.quinio <romain.quinio@gmail.com>
2015-01-02 16:26:12 +0100

    Replace %HOME% (non-standard env variable) with %HOMEDRIVE%%HOMEPATH%

--- create_symbolic_links.bat
+++ create_symbolic_links.bat
@@ -1,5 +1,6 @@
-mklink /J "%HOME%\\Documents\\Paradox Interactive\\Crusader Kings II\\mod\\WTWSMS" ".\\WTWSMS"
-mklink /J "%HOME%\\Documents\\Paradox Interactive\\Crusader Kings II\\mod\\WTWSMS_AfricanPortraits" ".\\WTWSMS_AfricanPortraits"
-mklink /J "%HOME%\\Documents\\Paradox Interactive\\Crusader Kings II\\mod\\WTWSMS_CelticPortraits" ".\\WTWSMS_CelticPortraits"
-mklink /J "%HOME%\\Documents\\Paradox Interactive\\Crusader Kings II\\mod\\WTWSMS_NorsePortraits" ".\\WTWSMS_NorsePortraits"
-mklink /J "%HOME%\\Documents\\Paradox Interactive\\Crusader Kings II\\mod\\WTWSMS_TurkishAndCelticPortraits" ".\\WTWSMS_TurkishAndCelticPortraits"
+mklink /J "%HOMEDRIVE%%HOMEPATH%\Documents\Paradox Interactive\Crusader Kings II\mod\WTWSMS" ".\WTWSMS"
+mklink /J "%HOMEDRIVE%%HOMEPATH%\Documents\Paradox Interactive\Crusader Kings II\mod\WTWSMS_AfricanPortraits" ".\WTWSMS_AfricanPortraits"
+mklink /J "%HOMEDRIVE%%HOMEPATH%\Documents\Paradox Interactive\Crusader Kings II\mod\WTWSMS_CelticPortraits" ".\WTWSMS_CelticPortraits"
+mklink /J "%HOMEDRIVE%%HOMEPATH%\Documents\Paradox Interactive\Crusader Kings II\mod\WTWSMS_NorsePortraits" ".\WTWSMS_NorsePortraits"
+mklink /J "%HOMEDRIVE%%HOMEPATH%\Documents\Paradox Interactive\Crusader Kings II\mod\WTWSMS_TurkishAndCelticPortraits" ".\WTWSMS_TurkishAndCelticPortraits"
+pause
