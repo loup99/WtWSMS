6adf5a2096efcd9b22a58568291a7c3d23982fb0
IhateTrains <IhateTrains@users.noreply.github.com>
2017-08-25 23:24:31 +0200

    reverting experimental fixes (history is being expanded in a separate branch)

--- WTWSMS/common/defines/WtWSMS_defines.lua
+++ WTWSMS/common/defines/WtWSMS_defines.lua
@@ -1,4 +1,3 @@
---NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 395 -- Set this to your earliest starting year  #awaiting 395 bookmark
 NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 475 -- Set this to your earliest starting year
 NDefines.NTechnology.IDEAL_YEAR_LEVEL_0 = 400  -- the chance to get a progress to this level will increase after this date and decrease before it
 NDefines.NTechnology.IDEAL_YEAR_LEVEL_8 = 1500
